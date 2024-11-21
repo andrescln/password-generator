875d6bee55f6c9fdbdaea4bf14148c1f564440a3
Andres Avendaño Aguilar <andrescyops@gmail.com>
2024-11-20 17:08:16 -0700

    NEW COMMENTS

--- password-generator.py
+++ password-generator.py
@@ -2,10 +2,10 @@ import tkinter as tk
 import random
 import sys
 import pyperclip
-
+#-------------------------------------------
 password = ""
-tuplas = []
-
+tuples = []
+#TUPLES
 lowercase = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
              "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
 
@@ -28,32 +28,18 @@ parenthesis = ('(', ')', '[', ']', '{', '}')
 minus = "-"
 underscore = "_"
 space = " "
-
+#-------------------------------------------
+#WINDOW CONFIGURATION/STYLE
 root = tk.Tk()
 root.title("PasswordGenerator") #Title
 root.geometry("370x270") #Size
 root.configure(bg="#1e1e1e")  # Dark background
 root.attributes("-alpha", 0.9) #Transparency
-
-check_style = {
-    "bg": "#1e1e1e",
-    "fg": "#bbbbbb",
-    "selectcolor": "#444444",
-    "activebackground": "#1e1e1e",
-    "activeforeground": "#ffffff",
-    "relief": tk.FLAT
-}
-
-bnt_style = {
-    "bg": "#444444",
-    "fg": "#ffffff",
-    "relief": tk.FLAT
-}
-
+#-------------------------------------------
 # Password Generator
-def password_generator(password, tuplas):
+def password_generator(password, tuples):
     for i in range(int(length_txt.get())):
-        password += random.choice(random.choice(tuplas))
+        password += random.choice(random.choice(tuples))
     return password
 
 def change_length_block(password):
@@ -63,35 +49,45 @@ def change_length_block(password):
     length_block.insert(0, password) #Insert Password
     length_block.config(state="readonly", fg="black") #Block TexBox
 
-#random tupla generator
-def random_tupla(tuplas):  
+#random Tuple generator
+def random_tuple(tuples):  
     if lowercase_var.get():
-        tuplas.append(lowercase)
+        tuples.append(lowercase)
     if capital_letters_var.get():
-        tuplas.append(capital_letters)
+        tuples.append(capital_letters)
     if numbers_var.get():
-        tuplas.append(numbers)
+        tuples.append(numbers)
     if special_var.get():
-        tuplas.append(special)
+        tuples.append(special)
     if ISO8859_var.get():
-        tuplas.append(Special_ISO8859)
+        tuples.append(Special_ISO8859)
     if parenthesis_var.get():
-        tuplas.append(parenthesis)
+        tuples.append(parenthesis)
     if minus_var.get():
-        tuplas.append(minus)
+        tuples.append(minus)
     if underscore_var.get():
-        tuplas.append(underscore)
+        tuples.append(underscore)
     if space_var.get():
-        tuplas.append(space)
+        tuples.append(space)
         
 def clipboard():
     pyperclip.copy(length_block.get())
     
-def check_config(password, tuplas):
-    random_tupla(tuplas)
-    password = password_generator(password, tuplas)
+def check_config(password, tuples):
+    random_tuple(tuples)
+    password = password_generator(password, tuples)
     change_length_block(password)
-
+#-------------------------------------------
+# CHECK BOX STYLE
+check_style = {
+    "bg": "#1e1e1e",
+    "fg": "#bbbbbb",
+    "selectcolor": "#444444",
+    "activebackground": "#1e1e1e",
+    "activeforeground": "#ffffff",
+    "relief": tk.FLAT
+}
+# CHECK BOX STATUS(ON/OFF)
 lowercase_var = tk.IntVar(value=1)
 capital_letters_var = tk.IntVar(value=1)
 numbers_var = tk.IntVar(value=1)
@@ -101,7 +97,7 @@ parenthesis_var = tk.IntVar(value=1)
 minus_var = tk.BooleanVar()
 underscore_var = tk.BooleanVar()
 space_var = tk.IntVar(value=1)
-
+# CHECK BOX
 lowercase_check = tk.Checkbutton(root, text="abcdef...vwxy", variable=lowercase_var, **check_style)
 capital_letters_check = tk.Checkbutton(root, text="ABCDEF...VWXY", variable=capital_letters_var, **check_style)
 numbers_check = tk.Checkbutton(root, text="0123456789", variable=numbers_var, **check_style)
@@ -111,23 +107,29 @@ parenthesis_check = tk.Checkbutton(root, text="()[]{}", variable=parenthesis_var
 minus_check = tk.Checkbutton(root, text="-", variable=minus_var, **check_style)
 underscore_check = tk.Checkbutton(root, text="_", variable=underscore_var, **check_style)
 space_check = tk.Checkbutton(root, text=" (space)", variable=space_var, **check_style)
-bnt_start = tk.Button(root, text="Create", command=lambda: check_config(password, tuplas), **bnt_style)
+#-------------------------------------------
+# BUTTON STYLE
+bnt_style = {
+    "bg": "#444444",
+    "fg": "#ffffff",
+    "relief": tk.FLAT
+}
+# BUTTON
+bnt_start = tk.Button(root, text="Create", command=lambda: check_config(password, tuples), **bnt_style)
 bnt_test = tk.Button(root, text="COPY", command=clipboard, **bnt_style)
-
-# Label Style
+#-------------------------------------------
+# LABEL STYLE
 label_style = {
     "bg": "#1e1e1e",  # Dark background
     "fg": "#bbbbbb",  # Light gray text
     "font": ("Arial", 12),  # Font
     "relief": tk.FLAT  # Flat border
 }
-
-#Label
+#LABEL
 text_label = tk.Label(root, text="Select u config", **label_style)
 length_label = tk.Label(root, text="Length of the password", **label_style)
-
-
-#Texbox Style
+#-------------------------------------------
+#TEX BOX STYLE
 entry_style = {
     "bg": "#1e1e1e",
     "fg": "#bbbbbb",
@@ -136,24 +138,22 @@ entry_style = {
     "highlightbackground": "#444444",
     "highlightthickness": 1 
 }
-
 # Texbox for length
 length_txt = tk.Entry(root, **entry_style)
 length_txt.insert(0, 30)
 # Texbox for copy
 length_block = tk.Entry(root, **entry_style)
 length_block.config(state="readonly") #Block, Only read
-
-
+#-------------------------------------------
 # List of Widgets with values of padx, pady and column (widget, padx, pady, column)
-# Tupla of Widgets for first column
+# Tuple of Widgets for first column
 column1_widgets = [
     (text_label, 10, 0, 0),(lowercase_check, 10, 0, 0), (capital_letters_check, 10, 0, 0), (numbers_check, 10, 0, 0),
     (special_check, 10, 0, 0), (ISO8859_check, 10, 0, 0), (parenthesis_check, 10, 0, 0),
     (minus_check, 10, 0, 0), (underscore_check, 10, 0, 0), (space_check, 10, 0, 0)
 ]
 
-# Tupla of Widgets for second column
+# Tuple of Widgets for second column
 column2_widgets = [
     (length_label, 10, 2, 1), (length_txt, 10, 2, 1), (length_block, 10, 2 ,1),
     (bnt_start, 10, 2, 1), (bnt_test, 10, 2, 1)
